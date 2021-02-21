e11b06399cdfc9dc88e46feb8fae4f9903bb4284
gilpgbd <72158669+gilpgbd@users.noreply.github.com>
2021-02-20 21:23:55 -0600

    Agrega tieneRol

--- js/CtrlPasatiempoNuevo.js
+++ js/CtrlPasatiempoNuevo.js
@@ -2,8 +2,7 @@ import {
   configuraSubmit, recibe
 } from "../lib/servicios.js";
 import {
-  iniciaSesión,
-  noAutorizado
+  tieneRol
 } from "./seguridad.js";
 
 /** @type {HTMLFormElement} */
@@ -11,21 +10,9 @@ const forma = document["forma"];
 
 protege();
 async function protege() {
-  const usuario =
-    await recibe(fetch(
-      "srv/sesion.php"));
-  if (usuario && usuario.cue) {
-    const roles = new Set(
-      usuario.rolIds || []);
-    if (roles.
-      has("Cliente")) {
+  if (await tieneRol(["Cliente"])) {
       configuraSubmit(forma,
         "srv/pasatiempoAgrega.php",
         "pasatiempos.html");
-    } else {
-      noAutorizado();
-    }
-  } else {
-    iniciaSesión();
   }
 }
